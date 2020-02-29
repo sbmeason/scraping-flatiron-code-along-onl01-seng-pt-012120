24cad6db343a0bc0693081e0df4e65d74039752e
Samantha Meason <sbmeason@gmail.com>
2020-02-29 21:01:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,10 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
   end
+  
+  def get_courses
+    self.get_page.css(".post")
+  end
 
 
 Scraper.new.get_page
